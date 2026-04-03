8e4afb93482e827d526f77efc0167e9dad524dd7
Thalia Webb <Thaliathenerder@gmail.com>
2026-04-03 12:10:58 -0400

    intake down speed 20%

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-bot-main";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 303;
-  public static final String GIT_SHA = "74304e4d22f41579d2e9fe476fc11d1e96d6bbbe";
-  public static final String GIT_DATE = "2026-03-31 22:00:16 EDT";
+  public static final int GIT_REVISION = 305;
+  public static final String GIT_SHA = "03e23fd831013955a126711c72789203347a8cc3";
+  public static final String GIT_DATE = "2026-04-01 15:22:01 EDT";
   public static final String GIT_BRANCH = "tw-unbrokened";
-  public static final String BUILD_DATE = "2026-03-31 22:13:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1775009581070L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-04-03 12:06:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1775232388458L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
