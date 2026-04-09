e0bd834f15ef991170ce0b4c2917bc17e4ca2aad
Thalia Webb <Thaliathenerder@gmail.com>
2026-04-09 09:54:35 -0400

    chore: update build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-bot-main";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 322;
-  public static final String GIT_SHA = "d7572da25e77ac11409e57ad791bd0a7c9450a7b";
-  public static final String GIT_DATE = "2026-04-06 08:07:53 EDT";
+  public static final int GIT_REVISION = 325;
+  public static final String GIT_SHA = "51e6da2b51c5406c0ae777b26c9902f7b455389e";
+  public static final String GIT_DATE = "2026-04-09 09:53:23 EDT";
   public static final String GIT_BRANCH = "tw-unbrokened";
-  public static final String BUILD_DATE = "2026-04-09 09:41:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1775742101705L;
+  public static final String BUILD_DATE = "2026-04-09 09:53:38 EDT";
+  public static final long BUILD_UNIX_TIME = 1775742818687L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
